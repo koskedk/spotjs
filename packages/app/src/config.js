c8b87d427c8ce5ad6af74581f207897230aaac51
Koske Kimutai <koskedk@live.com>
2019-08-22 22:48:33 +0300

    putting all together

--- packages/app/src/config.js
+++ packages/app/src/config.js
@@ -2,7 +2,7 @@ import * as isActive from './activityFns.js'
 import * as singleSpa from 'single-spa'
 
 singleSpa.registerApplication('navbar', () => SystemJS.import('@portal/navbar'), isActive.navbar)
-// singleSpa.registerApplication('globe', () => SystemJS.import('@portal/globe'), isActive.globe)
-// singleSpa.registerApplication('stats', () => SystemJS.import('@portal/stats'), isActive.stats)
+singleSpa.registerApplication('globe', () => SystemJS.import('@portal/globe'), isActive.globe)
+singleSpa.registerApplication('stats', () => SystemJS.import('@portal/stats'), isActive.stats)
 
 singleSpa.start()
